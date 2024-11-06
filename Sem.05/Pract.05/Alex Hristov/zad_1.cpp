96446d5e7aa50a7791d85a6cfc439673566b1900
alexhr05 <56800932+alexhr05@users.noreply.github.com>
2024-11-06 20:27:45 +0200

    Update zad_1.cpp Pract.05

--- Sem.05/Pract.05/Alex Hristov/zad_1.cpp
+++ Sem.05/Pract.05/Alex Hristov/zad_1.cpp
@@ -7,83 +7,43 @@ using namespace std;
 
 const int n = 1000;
 
-//double averageArr(int arr[], int n) {
-//	if (arr == NULL) {
-//		return;
-//	}
-//	double average = 0;
-//	for (int i = 0; i < n;i++) {
-//		average += arr[i];
-//	}
-//
-//	average /= n;
-//	return average;
-//
-//	
-//	
-//}
-//int closestToAverage(int arr[],int n) {
-//	double average = averageArr(arr,n);
-//	int closestToAverage = arr[0];
-//	for (int j = 0; j < n; j++) {
-//		if (abs(average - arr[j]) < abs(average - closestToAverage)) {
-//			closestToAverage = arr[j];
-//		}
-//	}
-//	
-//	return closestToAverage;
-//}
-
-//int maxEl(int a[], int n) {
-//	
-//	int max = a[0];
-//	for (int i = 1; i < n;i++) {
-//		if (a[i]>max) {
-//			max = a[i];
-//		}
-//		
-//	}
-//	return max;
-//}
-//int minEl(int a[], int n) {
-//	int min = a[0];
-//	
-//	for (int i = 1; i < n; i++) {
-//		
-//		if (a[i] < min) {
-//			min = a[i];
-//		}
-//	}
-//	return min;
-//}
-void removeElement(int arr[], int n, int k) {
-	const int capacity = n - 1;
-	for (int i = 0; i < n-1;i++) {
-		if (i >= k) {
-			arr[i] = arr[i + 1];
-		}
-		
+double averageArr(int arr[], int n) {
+	if (arr == NULL) {
+		return;
 	}
-	for (int i = 0; i < capacity; i++) {
-		cout << " " << arr[i];
+	double average = 0;
+	for (int i = 0; i < n;i++) {
+		average += arr[i];
 	}
 
+	average /= n;
+	return average;
+
+	
+	
 }
+int closestToAverage(int arr[],int n) {
+	double average = averageArr(arr,n);
+	int closestToAverage = arr[0];
+	for (int j = 0; j < n; j++) {
+		if (abs(average - arr[j]) < abs(average - closestToAverage)) {
+			closestToAverage = arr[j];
+		}
+	}
+	
+	return closestToAverage;
+}
+
 int main()
 {
-	int n, k;
+	int n;
 	cout << "Enter length of arr:";
 	cin >> n;
-	cout << "Enter index:";
-	cin >> k;
 	int arr[1000];
 	for (int i = 0; i < n; i++) {
 		cin >> arr[i];
 	}
-	removeElement(arr, n,k);
-	/*cout << "Min element:" << minEl(arr, n)<<endl;
-	cout << "Max element:" << maxEl(arr, n)<<endl;*/
-	/*cout<<"Average:" <<averageArr(arr, n)<<endl;
-	cout << "Closest To Average element:" << closestToAverage(arr, n)<<endl;*/
+	cout<<"Average:" <<averageArr(arr, n)<<endl;
+	cout << "Closest To Average element:" << closestToAverage(arr, n)<<endl;
 
 }
