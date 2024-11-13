ab0fabde5ce62d991be74d9803fd727b8bc6a9f5
nikol05nikolova <nikol05nikolova@gmail.com>
2024-11-13 11:00:49 +0200

    Update Task01.cpp

--- Sem.05/MultiArr/nikol/Task01.cpp
+++ Sem.05/MultiArr/nikol/Task01.cpp
@@ -1,9 +1,9 @@
-int findSmallest(int arr[3][3])
+int findSmallest(int arr[N][N])
 {
     int smallest = arr[0][0];
-	for (int i = 0; i < 3; i++)
+	for (int i = 0; i < N; i++)
 	{
-		for (int j = 0; j < 3; j++)
+		for (int j = 0; j < N; j++)
 		{
 			if (arr[i][j] < smallest)
 			{
