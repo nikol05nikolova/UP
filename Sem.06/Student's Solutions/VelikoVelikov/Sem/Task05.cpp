6d93fb31c93a0119f1ac7d61ff9c4c96fff575df
bul-bbx <kratos20050129@gmail.com>
2024-11-27 00:58:43 +0200

    Create Task05.cpp

---
+++ Sem.06/Student's Solutions/VelikoVelikov/Sem/Task05.cpp
@@ -0,0 +1,28 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+bool isBinarySubstring(int n, int k)
+{
+    int kBits = 0;
+    int tempK = k;
+    while (tempK > 0) {
+        tempK >>= 1;
+        kBits++;
+    }
+    int mask = (1 << kBits) - 1;
+    while (n >= k) {
+        if ((n & mask) == k) {
+            return true;
+        }
+        n >>= 1;
+    }
+    return false;
+}
+
+int main()
+{
+    int n, k; cin >> n >> k;
+    cout << isBinarySubstring(n, k);
+}
