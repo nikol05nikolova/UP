367d0b107ecc175b2cc87a66cdea4cf17704b7e8
bul-bbx <kratos20050129@gmail.com>
2024-11-27 00:27:42 +0200

    Create Task11.cpp

---
+++ Sem.02/Pract.02/Student's Solutions/VelikoVelikov/Pract/Task11.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c, temp;
+    cin >> a >> b >> c;
+
+    if (a < b)
+    {
+        temp = a;
+        a = b;
+        b = temp;
+    }
+    if (a < c)
+    {
+        temp = a;
+        a = c;
+        c = temp;
+    }
+    if (b < c)
+    {
+        temp = b;
+        b = c;
+        c = temp;
+    }
+
+    cout << "The biggest number you can get is: " << a << b << c;
+}
