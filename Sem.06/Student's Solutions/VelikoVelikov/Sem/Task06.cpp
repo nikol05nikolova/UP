2609f872129d74c6745020c947dc255aa1301e69
bul-bbx <kratos20050129@gmail.com>
2024-11-27 00:58:46 +0200

    Create Task06.cpp

---
+++ Sem.06/Student's Solutions/VelikoVelikov/Sem/Task06.cpp
@@ -0,0 +1,29 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+void printSubsets(int arr[], int n)
+{
+    int totalSubsets = 1 << n;
+
+    for (int i = 0; i < totalSubsets; i++) {
+        cout << "[";
+        bool first = true;
+        for (int j = 0; j < n; j++) {
+            if (i & (1 << j)) {
+                if (!first) cout << ", ";
+                cout << arr[j];
+                first = false;
+            }
+        }
+        cout << "]" << endl;
+    }
+}
+
+int main()
+{
+    int arr[] = { 1, 2, 3 };
+    int size = 3;
+    printSubsets(arr, size);
+}
