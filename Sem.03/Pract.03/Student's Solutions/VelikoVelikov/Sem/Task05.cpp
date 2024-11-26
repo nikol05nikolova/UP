8e566193d505ede18c7057fb938cb1e19030a367
bul-bbx <kratos20050129@gmail.com>
2024-11-27 00:52:35 +0200

    Create Task05.cpp

---
+++ Sem.03/Pract.03/Student's Solutions/VelikoVelikov/Sem/Task05.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a, b; cin >> a >> b;
+	int tempA = a, tempB = b;
+	int gcd = 0;
+
+	while (tempB != 0)
+	{
+		int temp = tempB;
+		tempB = tempA % tempB;
+		tempA = temp;
+	}
+	gcd = tempA;
+
+	int lcm = (a * b) / gcd;
+
+	cout << lcm;
+}
