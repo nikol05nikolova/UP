addf301431c8bd8f40d71f51a4ff042061c85f2d
bul-bbx <kratos20050129@gmail.com>
2024-11-26 23:57:26 +0200

    Create Task09.cpp

---
+++ Sem.01/Pract.01/Student's Solutions/VelikoVelikov/Pract/Task09.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cout << "Enter a: ";
+    cin >> a;
+
+    cout << "Enter b: ";
+    cin >> b;
+
+    cout << "Enter c: ";
+    cin >> c;
+
+    double D = pow(b, 2) - 4 * a * c;
+
+    double x1 = (-1 * b + sqrt(D)) / (2 * a), x2 = (-1 * b - sqrt(D)) / (2 * a);
+
+    if (D > 0)
+    {
+        cout << "x1 = " << x1 << endl << "x2 = " << x2;
+    }
+    else
+    {
+        if (D == 0)
+        {
+            cout << "x = " << x1;
+        }
+        else
+        {
+            cout << "There are no real solutions to this equaion!";
+        }
+    }
+}
