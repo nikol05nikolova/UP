a208a618dc5faa75653e1b5c56db2397bc358c0b
bul-bbx <kratos20050129@gmail.com>
2024-11-27 00:27:40 +0200

    Create Task10.cpp

---
+++ Sem.02/Pract.02/Student's Solutions/VelikoVelikov/Pract/Task10.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double a, b;
+    char operation;
+    cin >> a >> b;
+
+    cout << "Choose the binary operation (+, -, *, /): ";
+    cin >> operation;
+
+    switch (operation)
+    {
+    case '+': cout << "The result is: " << a + b; break;
+    case '-': cout << "The result is: " << a - b; break;
+    case '*': cout << "The result is: " << a * b; break;
+    case '/':
+        if (b != 0) cout << "The result is: " << a / b;
+        else cout << "Nah fam you can't divide by 0, nuh-uh!";
+        break;
+    default: cout << "What is this operation you want to use homie?"; break;
+    }
+}
