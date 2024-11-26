6a37bef4428843be44e8491f4840deb24735be22
bul-bbx <kratos20050129@gmail.com>
2024-11-27 00:52:27 +0200

    Create Task01.cpp

---
+++ Sem.03/Pract.03/Student's Solutions/VelikoVelikov/Sem/Task01.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int sum = 0;
+    int input;
+    cin >> input;
+
+    while (input != 0)
+    {
+        sum += input;
+        cin >> input;
+    }
+
+    cout << sum;
+}
