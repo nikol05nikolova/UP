cd0788d38cfc18a54f20c058efd1c0865b33666b
bul-bbx <kratos20050129@gmail.com>
2024-11-26 23:57:28 +0200

    Create Task10.cpp

---
+++ Sem.01/Pract.01/Student's Solutions/VelikoVelikov/Pract/Task10.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float first, second;
+
+    cout << "Enter the first number: ";
+    cin >> first;
+
+    cout << "Enter the second number: ";
+    cin >> second;
+
+    float temp = first;
+
+    first = second;
+    second = temp;
+
+    cout << "The switched numbers are: " << endl
+        << "First - " << first << endl
+        << "Second - " << second << endl;
+}
