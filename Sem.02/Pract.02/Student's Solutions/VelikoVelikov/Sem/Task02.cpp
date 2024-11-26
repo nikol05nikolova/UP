b7e188c6c23d874421ce60bce58b7af7b4ff67b4
bul-bbx <kratos20050129@gmail.com>
2024-11-26 23:33:06 +0200

    Create Task02.cpp

---
+++ Sem.02/Pract.02/Student's Solutions/VelikoVelikov/Sem/Task02.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number;
+    cin >> number;
+
+    if (number > 200)
+    {
+        cout << "It's over two hundreeeeeed";
+    }
+    else if (number >= 100)
+    {
+        cout << "It's just between 100 and 200 sooo it's mid";
+    }
+    else
+    {
+        cout << "Naaah fam, it's only under 100 sooo... Ain't gon happen";
+    }
+}
+
