773f27c8bda026e4450e91056a47ab9b62df2e68
Atanas Hristov <44443630+AtanasHrist0v@users.noreply.github.com>
2024-12-11 10:59:27 +0000

    Update Task01.cpp

--- Sem.08/Solutions/Task01.cpp
+++ Sem.08/Solutions/Task01.cpp
@@ -1,5 +1,6 @@
 bool AnalyzeArr(int arr[], int length) {
-	if (length < 2 || arr == nullptr) return false;
+	const int ARR_MIN_LEN = 2;
+	if (length < ARR_MIN_LEN || arr == nullptr) return false;
 
 	bool foundFirstFixed = false;
 	for (int i = 0; i < length; i++) {
